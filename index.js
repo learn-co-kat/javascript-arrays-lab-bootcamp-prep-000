dd63d5f6e2f756ea3221927a63e756e8cbb19d94
Katrina R <katrina.reinsdorff@gmail.com>
2019-07-09 21:17:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
+
 function kittens () {
   return kittens
 }
 
+function destructivelyAppendKitten(name) {
+  kittens.push("Ralph");
+  return kittens 
+}
